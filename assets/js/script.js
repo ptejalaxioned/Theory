ba008c35ed0db8b76b0d21d983bcf159ee28080b
ptejalaxioned <tejalp@axioned.com>
2024-06-10 19:34:10 +0530

    js file commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,18 @@
+let nav=document.querySelector(".hamburger")
+let nav_list=document.querySelector(".nav-list")
+let line1 = document.querySelector(".line1");
+let line2 = document.querySelector(".line2");
+let line3 = document.querySelector(".line3");
+let line = document.querySelector(".line");
 
+nav.addEventListener("click",()=>{
+    nav_list.classList.toggle("nav-list-onclick")
+    line1.classList.toggle("line1-onclick");
+    line2.classList.toggle("line2-onclick");
+    line3.classList.toggle("line3-onclick");
+    document.childNodes[1].classList.toggle("body-onclick")
+console.log(document.childNodes[1])
+})
 
 
 
